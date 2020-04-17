c63967f69cd3b82c601d3e50d16edf127b42b21d
mysterious-Palace <60834599+mysterious-Palace@users.noreply.github.com>
2020-04-17 20:34:06 +0800

    Add files via upload

---
+++ js/1.js
@@ -0,0 +1,11 @@
+var main=document.getElementById("main");
+
+main.innerHTML+="欢迎来到45dino Nazo。<br/>\n";
+main.innerHTML+="我想你应该拿到了一个exe文件，只要输入每一关的答案，就会显示下一关的网址。<br/>\n";
+main.innerHTML+="请不要跳关。<br/>\n";
+main.innerHTML+="建议不要关掉这个exe文件，否则你就会从头开始。<br/>\n";
+main.innerHTML+="如果你输入网址后变乘了404，说明你写错了。<br/>\n";
+main.innerHTML+="所有关卡的答案都是英文小写字母或数字，<b>没有空格</b>，如果是中文请把它写成拼音，而且会在关卡号上有所标记，比如这一关。<br/>\n";
+main.innerHTML+="会出现Hint之类的东西，对解题很有帮助！<br/>\n";
+main.innerHTML+="Hint：蒟蒻<br/>\n";
+main.innerHTML+="有<img src=\"http://www.hit-counts.com/counter.php?t=MTQ0NzUxOA==\">人来过这里<br/>\n";
---
+++ js/init.js
@@ -0,0 +1,12 @@
+var da=
+[
+  "juruo",
+  "27946",
+  "4",
+  "akioi",
+  "slfn",
+  "34886",
+  "250637",
+  "45dinotlep1000",
+  "haveagoodtime",
+]
